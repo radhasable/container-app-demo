5352ae23f48d6d05a71c34a29d3fe18abe9d3f70
radhasable <radha.sable25@gmail.com>
2023-09-09 20:38:15 +0530

    updated cat image and index.js

--- azure-container-demo/pages/index.js
+++ azure-container-demo/pages/index.js
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
+function HoverableText({ text }) {
+  return (
+    <>
+      {text.split("").map((char, index) => (
+        <span key={index} className={styles.hoverableLetter}>
+          {char}
+        </span>
+      ))}
+    </>
+  );
+}
+
 export default function Home() {
   const [hovered, setHovered] = useState(false);
 
@@ -14,8 +26,19 @@ export default function Home() {
         Welcome to SynthWave '84
       </header>
       <main className={styles.mainContent}>
-        <p>Did you know that keyboards in the '80s had a mind of their own? 🎹✨</p>
-        <p>Stay awhile and enjoy the neon glow!</p>
+        <img src="/cat_image2.jpg" alt="Minimalistic Cat" className={styles.catImage} />
+        <div className={styles.imageCredit}>
+          Photo by 
+          <a href="https://unsplash.com/@feneek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Sergey Semin
+          </a> 
+          on 
+          <a href="https://unsplash.com/photos/NgifUHDH5sQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </div>
+        <p><HoverableText text="Did you know that keyboards in the '80s had a mind of their own? 🎹✨" /></p>
+        <p><HoverableText text="Stay awhile and enjoy the neon glow!" /></p>
       </main>
       <footer className={styles.footer}>
         Made with love and a pinch of neon.
