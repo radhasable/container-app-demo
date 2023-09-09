4321396cae367f3f03f098377bfda031a693bc46
radhasable <radha.sable25@gmail.com>
2023-09-09 23:33:40 +0530

    updated index.js

--- azure-container-demo/pages/index.js
+++ azure-container-demo/pages/index.js
@@ -1,31 +1,27 @@
 import styles from './index.module.css';
 
 export default function Home() {
-  const [hovered, setHovered] = useState(false);
-
   return (
     <div className={styles.container}>
-      <header
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className={hovered ? styles.headerHovered : styles.header}
-      >
+      <header className={styles.header}>
         Welcome to Tabby Town '84
       </header>
       <main className={styles.mainContent}>
         <img src="/cat_image2.jpg" alt="Tabby Cat" className={styles.catImage} />
         <div className={styles.imageCredit}>
-          Photo by 
+          Photo by&nbsp;
           <a href="https://unsplash.com/@feneek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-             Sergey Semin 
-          </a> 
-          on 
+            Sergey Semin
+          </a>&nbsp;
+          on&nbsp;
           <a href="https://unsplash.com/photos/NgifUHDH5sQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-             Unsplash 
+            Unsplash
           </a>
         </div>
-        <p>Did you know that keyboards in the '80s had a purr-fect rhythm? 🎹✨</p>
-        <p>Stay a-while and enjoy the meow-gical glow!</p>
+        <p>In Tabby Town, where whiskers glow,</p>
+        <p>Cats dance to beats, both high and low.</p>
+        <p>With tails that flick and eyes that gleam,</p>
+        <p>They purr and prance in neon dreams.</p>
       </main>
       <footer className={styles.footer}>
         Made with paws and a touch of meow.
