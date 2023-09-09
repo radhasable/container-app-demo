80592460e846330b2e36cac2b129b5656b78bfb5
radhasable <radha.sable25@gmail.com>
2023-09-09 21:09:57 +0530

    updated index.js removed hover function

--- azure-container-demo/pages/index.js
+++ azure-container-demo/pages/index.js
@@ -1,21 +1,4 @@
-import { useState } from 'react';
 import styles from './index.module.css';
-
-function HoverableText({ text }) {
-    const chars = text.split("").map((char, index) => {
-      if (char === ' ') {
-        return <span key={index}>&nbsp;</span>;
-      }
-      return (
-        <span key={index} className={styles.hoverableLetter}>
-          {char}
-        </span>
-      );
-    });
-  
-    return <>{chars}</>;
-  }
-  
 
 export default function Home() {
   const [hovered, setHovered] = useState(false);
@@ -27,25 +10,25 @@ export default function Home() {
         onMouseLeave={() => setHovered(false)}
         className={hovered ? styles.headerHovered : styles.header}
       >
-        Welcome to SynthWave '84
+        Welcome to Tabby Town '84
       </header>
       <main className={styles.mainContent}>
-        <img src="/cat_image2.jpg" alt="Minimalistic Cat" className={styles.catImage} />
+        <img src="/cat_image2.jpg" alt="Tabby Cat" className={styles.catImage} />
         <div className={styles.imageCredit}>
           Photo by 
           <a href="https://unsplash.com/@feneek?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-            Sergey Semin
+             Sergey Semin 
           </a> 
           on 
           <a href="https://unsplash.com/photos/NgifUHDH5sQ?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-            Unsplash
+             Unsplash 
           </a>
         </div>
-        <p><HoverableText text="Did you know that keyboards in the '80s had a mind of their own? 🎹✨" /></p>
-        <p><HoverableText text="Stay awhile and enjoy the neon glow!" /></p>
+        <p>Did you know that keyboards in the '80s had a purr-fect rhythm? 🎹✨</p>
+        <p>Stay a-while and enjoy the meow-gical glow!</p>
       </main>
       <footer className={styles.footer}>
-        Made with love and a pinch of neon.
+        Made with paws and a touch of meow.
       </footer>
     </div>
   );
